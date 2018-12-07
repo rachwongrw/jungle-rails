991d250c716710c9407b283afc18ee25af3c0b33
Rachel Wong <rwrachelwong@gmail.com>
2018-12-07 04:50:12 +0000

    Feature: E-mail Receipt

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
   layout 'mailer'
-
-  def order_confirmation(user, order)
-    @user = user
-    @order = order
-    mail(to: user.email, subject: 'Order #{@order.id} has been received.')
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  def order_confirmation(email, order)
+    @email = email
+    @order = order
+    mail(to: email, subject: "Order ##{@order.id} confirmed.")
+  end
+
 end
