39b41b8098a5408595075c4f97ed04da05492eea
Rachel Wong <rwrachelwong@gmail.com>
2018-12-11 01:11:13 +0000

    add new authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ActiveRecord::Base
   validates :email,                 uniqueness: true
   validates :password,              presence: true
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user
+      if user.authenticate(password)
+        user
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,22 @@ RSpec.describe User, type: :model do
     end
 
     it "is not valid without a unique email" do
-      expect(@user.email).not_to be_nil
+      new_user = User.create(
+        first_name: 'Jane',
+        last_name: 'Doe',
+        email: 'jane@doe.me',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+
+      new_user2 = User.create(
+        first_name: 'Jane',
+        last_name: 'Doe',
+        email: 'jane@doe.me',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+      expect(new_user2).not_to be_valid
     end
 
     it "is not valid without a password" do
@@ -41,6 +56,18 @@ RSpec.describe User, type: :model do
       @user.password = 'as'
       expect(@user.password.length).not_to be >= 6
     end
+  end
 
+  describe '.authenticate_with_credentials' do
+    it "should be an existing user" do
+
+      described_class.authenticate_with_credentials(email, password)
+      expect(login.errors).to be_empty
+
+
+      described_class.authenticate_with_credentials(email, password)
+      expect(login.errors.first). to eq "must be an existing user"
+
+    end
   end
 end
